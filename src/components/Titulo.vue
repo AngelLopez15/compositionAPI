8c05d2a4c5975b74240c9503bb230f731e17474c
Angel Octavio Lopez <a.o.lopez.cruz@gmail.com>
2020-11-22 13:30:14 -0600

    Componente titulo agregado

---
+++ src/components/Titulo.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1
+    :style="{'color':color}"
+  >Titulo Dinamico:{{contador}}</h1>
+  <h1
+    :style="{'color':color}"
+  >Titulo Dinamico2: {{signoPeso}}</h1>
+</template>
+
+<script>
+import { computed } from 'vue'
+export default {
+  // Los props se sigue trabajando de la misma manera que en vue 2
+  props:['contador', 'color'],
+  // podemos combinar las propiedades computadas con el prop de otro componente
+  // Podemos mezclar el API de composicion con el API de opciones
+  // computed:{
+  //   signoPeso(){
+  //     return '$' + this.contador
+  //   }
+  // }
+  
+  // Haciendo el mismo computed pero con setup
+  // para acceder a contador debemos de pasar los props
+  setup(props){
+    const signoPeso = computed(()=>{
+      return '$' + props.contador
+    })
+
+    return {signoPeso}
+  }
+
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,75 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <!-- Pasando props al componente para que sea dinamico el titulo -->
+    <Titulo :contador="contador" :color="color" />
+    <h1
+      :style="{'color':color}"
+    >
+      Contador:{{contador}}
+    </h1>
+    <button @click="aumentar">+</button>
+    <button @click="disminuir">-</button>
+
+    <hr>
+
+    <input type="text" v-model="texto">
+    <p>{{texto}}</p>
   </div>
 </template>
+
+<script>
+// ref se necesita para que nuestros valores sean reactivos
+import { computed, ref } from 'vue'
+import Titulo from '../components/Titulo'
+// Asi seria trabajar el contador con la API de composicion
+export default {
+  // tenemos el setup. El setup se ejecuta antes de que se cree el componente,
+  // una vez que se resuelven los props y sirve como punto de entrada de la composition API
+  // Una ventaja es que dentro del setup podemos hacer un llamado a una API externa
+  // antes de que se pinte toda la informacion del template.
+
+  // Los componentes se digue trabajando igual que en vue 2
+  components:{
+    Titulo
+  },
+
+  // En el setup NO se incorporan los componentes, No se incorporan los props, No se incorpora
+  // el nombre de la vista
+
+  // El setup SI tiene todas las configuraciones de las propiedades computadas, los metodos
+  // El ciclo de vida
+  setup(){
+    // para que nuestro contador sea reactivo debemos crear una referencia
+    // entonces debemos hacer uso del metodo ref
+    // ref puede recibir numeros, booleanos, objetos, arreglos. 
+    const contador = ref(0)
+
+    // Para los declarar los metodos ya no necesitamos usar la palabra methods 
+    // simplemente creamos funciones
+    const aumentar = () => {
+      // para acceder al valor de la referencia debemos acceder con el operador punto
+      contador.value ++
+    }
+
+    const disminuir = () => {
+      contador.value --
+    }
+
+    // para hacer uso de las propiedades computadas en vue 3 debemos crear una constante
+    const color = computed(()=>{
+      // Recordar que para acceder al valor de contador debemos usar el .value
+      if (contador.value<0) {
+        return 'red'
+      }
+      return 'blue'
+    })
+
+    // para usar el v-model en vue 3
+    const texto = ref('')
+    
+    
+    return { contador, aumentar, disminuir, color, texto }
+  }
+
+}
+</script>
